4c03582a3f9353356af3b8d4827b7f7682d35c78
OriVered <ori.vered@gmail.com>
2023-05-19 18:16:59 +0300

    Extract from pptx

--- engine.py
+++ engine.py
@@ -1,7 +1,7 @@
 from pptx_processor import extract_text_from_presentation
 
 
-
 file_path = 'End of course exercise - kickof - upload.pptx'
 slides_data = extract_text_from_presentation(file_path)
+
 print(slides_data)
--- pptx_processor.py
+++ pptx_processor.py
@@ -2,47 +2,74 @@ from pptx import Presentation
 from pptx.exc import PackageNotFoundError
 from typing import List, Dict, Optional
 
-
 class PPTXProcessor:
     def __init__(self, presentation: Presentation):
         self.presentation = presentation
 
     @staticmethod
-    def _extract_text_from_slide(slide) -> Dict[str, str]:
-        slide_data = {"title": "", "subtitle": "", "body": ""}
+    def _extract_text_from_shape(shape) -> Optional[str]:
+        """Extracts text from a single shape."""
+        if not shape.has_text_frame:
+            return None
+        return shape.text
+
+    @staticmethod
+    def _extract_text_from_table(shape) -> Optional[List[str]]:
+        """Extracts text from a table shape."""
+        if not shape.has_table:
+            return None
+        return [cell.text for row in shape.table.rows for cell in row.cells if cell.text]
+
+    @staticmethod
+    def _extract_text_from_smartart(shape) -> Optional[List[str]]:
+        """Extracts text from a SmartArt shape."""
+        if shape.shape_type != 14:  # Shape type 14 corresponds to SmartArt
+            return None
+
+        smartart_text = []
+        for i in range(shape.graphic_frame.graphic.graphic_data.element.child_len()):
+            node = shape.graphic_frame.graphic.graphic_data.element.getchildren()[i]
+            for paragraph in node.xpath('.//a:t', namespaces={'a': 'http://schemas.openxmlformats.org/drawingml/2006/main'}):
+                smartart_text.append(paragraph.text)
+        return smartart_text
+
+    def _extract_text_from_slide(self, slide) -> Dict[str, str]:
+        """Extracts text, tables, and SmartArt from a slide."""
+        slide_data = {"title": "", "subtitle": "", "body": "", "tables": [], "smartart": []}
 
         for shape in slide.shapes:
-            if shape.has_text_frame:
-                if shape == slide.shapes.title:
-                    slide_data["title"] = shape.text
-                elif len(slide.placeholders) > 1 and shape == slide.placeholders[
-                    1]:  # Check for existence of placeholder 1
-                    slide_data["subtitle"] = shape.text
-                else:
-                    slide_data["body"] += shape.text + "\n"
+            if shape == slide.shapes.title:
+                slide_data["title"] = self._extract_text_from_shape(shape)
+            elif len(slide.placeholders) > 1 and shape == slide.placeholders[1]:
+                slide_data["subtitle"] = self._extract_text_from_shape(shape)
+            else:
+                text = self._extract_text_from_shape(shape)
+                if text:
+                    slide_data["body"] += text + "\n"
+
+                table = self._extract_text_from_table(shape)
+                if table:
+                    slide_data["tables"].append(table)
+
+                smartart = self._extract_text_from_smartart(shape)
+                if smartart:
+                    slide_data["smartart"].append(smartart)
 
         return slide_data
 
     def extract_text(self) -> List[Dict[str, str]]:
+        """Extracts text, tables, and SmartArt from all slides in the presentation."""
         return [self._extract_text_from_slide(slide) for slide in self.presentation.slides]
 
-
 def load_presentation(file_path: str) -> Presentation:
+    """Loads a PowerPoint presentation from a file."""
     try:
         return Presentation(file_path)
     except PackageNotFoundError:
         raise ValueError(f"File not found at the path: {file_path}")
 
-
 def extract_text_from_presentation(file_path: str) -> List[Dict[str, str]]:
-    """Extracts text from a PowerPoint presentation.
-
-    Args:
-        file_path: The path to the PowerPoint file.
-
-    Returns:
-        A list of dictionaries containing the title, subtitle, and body text from each slide.
-    """
+    """Extracts text, tables, and SmartArt from a PowerPoint presentation."""
     presentation = load_presentation(file_path)
     processor = PPTXProcessor(presentation)
     return processor.extract_text()

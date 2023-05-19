18f069dba5875385b746d3af30579398ba046156
OriVered <ori.vered@gmail.com>
2023-05-19 18:35:19 +0300

    prompt composer

--- engine.py
+++ engine.py
@@ -1,7 +1,6 @@
 from pptx_processor import extract_text_from_presentation
-
+from prompt_composer import prompt_composer
 
 file_path = 'End of course exercise - kickof - upload.pptx'
 slides_data = extract_text_from_presentation(file_path)
-
-print(slides_data)
+prompt = prompt_composer(slides_data)

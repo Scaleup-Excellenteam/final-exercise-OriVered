8fe9ad39b528c0e579c21c9cc08a3f0e485d62ff
OriVered <ori.vered@gmail.com>
2023-06-07 18:30:39 +0300

    added web api requests

---
+++ app/web_api.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+import os
+import uuid
+import json
+from datetime import datetime
+
+# Constants
+CURRENT_DIR = os.getcwd()
+UPLOAD_FOLDER = os.path.join(CURRENT_DIR, 'uploads')
+OUTPUT_FOLDER = os.path.join(CURRENT_DIR, 'outputs')
+STATUS_DONE = 'done'
+STATUS_PENDING = 'pending'
+STATUS_NOT_FOUND = 'not found'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    """
+    Upload endpoint:
+    - Receives a POST request with an attached file.
+    - Generates a UID for the uploaded file.
+    - Saves the file in the `uploads` folder with the original filename, a timestamp, and the UID.
+    - Returns a JSON object with the UID of the upload.
+    """
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
+    if 'file' not in request.files:
+        return jsonify(error='No file part'), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify(error='No selected file'), 400
+    if file:
+        uid = str(uuid.uuid4())
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = secure_filename(file.filename)
+        new_filename = f"{filename}_{timestamp}_{uid}"
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
+        return jsonify(uid=uid), 200
+
+@app.route('/status/<uid>', methods=['GET'])
+def check_status(uid):
+    """
+    Status endpoint:
+    - Receives a GET request with a UID as a URL parameter.
+    - Returns a JSON object with the status, original filename, timestamp, and explanation of the upload.
+    """
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+
+    files = os.listdir(OUTPUT_FOLDER)
+    for file in files:
+        if uid in file:
+            with open(os.path.join(OUTPUT_FOLDER, file), 'r') as f:
+                data = json.load(f)
+            original_filename, timestamp, _ = file.rsplit('_', 2)
+            return jsonify(status=STATUS_DONE, filename=original_filename, timestamp=timestamp, explanation=data), 200
+    files = os.listdir(UPLOAD_FOLDER)
+    for file in files:
+        if uid in file:
+            original_filename, timestamp, _ = file.rsplit('_', 2)
+            return jsonify(status=STATUS_PENDING, filename=original_filename, timestamp=timestamp, explanation=None), 200
+    return jsonify(status=STATUS_NOT_FOUND, filename=None, timestamp=None, explanation=None), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
